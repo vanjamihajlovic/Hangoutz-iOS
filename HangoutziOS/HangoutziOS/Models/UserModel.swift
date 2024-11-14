bc9f0128f3e597e48527ffaf4799743a29084bcc
Vanja Mihajlović <73716807+vanjamihajlovic@users.noreply.github.com>
2024-11-14 14:14:09 +0100

    Added folders: Views, Models and ModelViews

---
+++ HangoutziOS/HangoutziOS/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  HangoutziOS
+//
+//  Created by User01 on 11/14/24.
+//
+
+import Foundation
---
+++ HangoutziOS/HangoutziOS/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  HangoutziOS
+//
+//  Created by User01 on 11/14/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
