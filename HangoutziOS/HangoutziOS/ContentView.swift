9d53eab4f2cbb3e5751d6adf7324caf43e7ede96
Vanja Mihajlović <73716807+vanjamihajlovic@users.noreply.github.com>
2024-11-14 11:54:43 +0100

    initial Commit XCode project setup

---
+++ HangoutziOS/HangoutziOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HangoutziOS
+//
+//  Created by User01 on 11/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HangoutziOS/HangoutziOS/HangoutziOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangoutziOSApp.swift
+//  HangoutziOS
+//
+//  Created by User01 on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HangoutziOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
