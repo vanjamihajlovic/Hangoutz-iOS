e17848641681218ccea139e1fdb61dc3efc9385b
alex64a <aleksamitrovcan@gmail.com>
2024-11-17 12:24:55 +0100

    Added struct UserData in /Models folder, created by quicktype. Added DownloadUserData class for downloading all users.

--- HangoutziOS/HangoutziOS/Models/UserModel.swift
+++ HangoutziOS/HangoutziOS/Models/UserModel.swift
@@ -2,7 +2,28 @@
 //  UserModel.swift
 //  HangoutziOS
 //
-//  Created by User01 on 11/14/24.
+//  Edited by alex64a on 11/17/24.
 //
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
 
 import Foundation
+
+// MARK: - userData
+struct userData: Codable {
+    let name: String?
+    let avatar: String?
+    let id, email: String?
+    let passwordHash: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name, avatar, id, email
+        case passwordHash = "password_hash"
+    }
+}
+
+typealias Welcome = [userData]
+
+
---
+++ HangoutziOS/HangoutziOS/ViewModels/DownloadUserData.swift
@@ -0,0 +1,53 @@
+//
+//  downloadUserData.swift
+//  HangoutziOS
+//
+//  Created by alex64a on 11/17/24.
+//
+
+import Foundation
+
+class DownloadUserData: ObservableObject {
+    
+    //array of type struct userData
+    @Published var users: [userData] = []
+    
+    init() {
+        getUserData()
+    }
+    
+    func getUserData() {
+        
+        guard let url = URL(string: "https://zsjxwfjutstrybvltjov.supabase.co/rest/v1/users?select=*") else { return }
+        
+        downloadData(fromURL: url) { (returnedData) in
+            if let data = returnedData {
+                guard let newUsers = try? JSONDecoder().decode([userData].self, from: data) else { return }
+                DispatchQueue.main.async { [weak self] in
+                    self?.users = newUsers
+                }
+            } else {
+                print("No data returned.")
+            }
+        }
+    }
+    
+    func downloadData(fromURL url: URL, completionHandler: @escaping (_ data: Data?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                print("Error downloading data.")
+                completionHandler(nil)
+                return
+            }
+            
+            completionHandler(data)
+            
+        }.resume()
+    }
+
+}
