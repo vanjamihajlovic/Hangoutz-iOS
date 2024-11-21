7548d5fbc4d353fbc03c2668d50b24928b65210b
Vanja Mihajlović <73716807+vanjamihajlovic@users.noreply.github.com>
2024-11-21 11:07:38 +0100

    small stuff, idk

--- HangoutziOS/HangoutziOS/Views/RegistrationView.swift
+++ HangoutziOS/HangoutziOS/Views/RegistrationView.swift
@@ -17,51 +17,51 @@ struct RegistrationView: View {
                     .padding(.bottom,60)
                 VStack(alignment: .leading, spacing: 5) {
                     TextField("", text: $registrationViewModel.nameRegistration,
-                               prompt: Text("Name")
-                                    .foregroundColor(.white)
-                            )
+                              prompt: Text("Name")
+                        .foregroundColor(.white)
+                    )
                     .autocapitalization(.none)
                     .frame(width: 320, height: 25, alignment: .center)
                     .foregroundColor(.white)
                     .padding()
                     .overlay(
-                            RoundedRectangle(cornerRadius: 20)
+                        RoundedRectangle(cornerRadius: 20)
                             .stroke(registrationViewModel.isNameValid ? Color.white : Color("ErrorColor"), lineWidth: 3)
                     )
-                                    
+                    
                     if !registrationViewModel.isNameValid && registrationViewModel.allFieldsFilled {
                         Text("Name must be between 3 and 25 characters.")
-                        .foregroundColor(Color.white)
-                        .font(.system(size: 17))
-                        .padding(.leading, 5)
-                        }
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 17))
+                            .padding(.leading, 5)
+                    }
                 }
                 
                 VStack(alignment: .leading, spacing: 5) {
                     TextField("", text: $registrationViewModel.emailRegistration, prompt: Text("Email")
-                             .foregroundColor(.white)
-                            )
+                        .foregroundColor(.white)
+                    )
                     .autocapitalization(.none)
                     .frame(width: 320, height: 25, alignment: .center)
                     .foregroundColor(.white)
                     .padding()
                     .overlay(
-                            RoundedRectangle(cornerRadius: 20)
+                        RoundedRectangle(cornerRadius: 20)
                             .stroke(registrationViewModel.isEmailValid ? Color.white : Color("ErrorColor"), lineWidth: 3)
-                            )
+                    )
                     if !registrationViewModel.isEmailValid && registrationViewModel.allFieldsFilled{
-                                Text("Enter a valid email address.")
+                        Text("Enter a valid email address.")
                             .foregroundColor(Color.white)
-                                .font(.system(size: 17))
-                                .padding(.leading, 5)
-                                    }
-                 }
+                            .font(.system(size: 17))
+                            .padding(.leading, 5)
+                    }
+                }
                 
                 VStack(alignment: .leading, spacing: 5) {
                     SecureField("", text: $registrationViewModel.passwordRegistration,
-                        prompt: Text("Password")
+                                prompt: Text("Password")
                         .foregroundColor(.white)
-                                )
+                    )
                     .textContentType(.password)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
@@ -70,22 +70,20 @@ struct RegistrationView: View {
                     .foregroundColor(.white)
                     .padding()
                     .overlay(
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(registrationViewModel.isPasswordValid ? Color.white : Color("ErrorColor"), lineWidth: 3)
-                                    )
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(registrationViewModel.isPasswordValid ? Color.white : Color("ErrorColor"), lineWidth: 3)
+                    )
                     if !registrationViewModel.isPasswordValid && registrationViewModel.allFieldsFilled {
-                    Text("At least 8 characters,a digit")
+                        Text("At least 8 characters,a digit")
                             .foregroundColor(Color.white)
-                    .font(.system(size: 17))
-                    .padding(.leading, 5)
-                                    }
+                            .font(.system(size: 17))
+                            .padding(.leading, 5)
+                    }
                 }
-                
-                
                 VStack(alignment: .leading, spacing: 5) {
                     SecureField("", text: $registrationViewModel.password2Registration, prompt: Text("Re-enter password")
                         .foregroundColor(.white)
-                                 )
+                    )
                     .autocapitalization(.none)
                     .textContentType(.password)
                     .frame(width: 320, height: 25, alignment: .center)
@@ -93,20 +91,20 @@ struct RegistrationView: View {
                     .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 20)
-                        .stroke(registrationViewModel.isPassword2Valid ? Color.white : Color("ErrorColor"), lineWidth: 3)
+                            .stroke(registrationViewModel.isPassword2Valid ? Color.white : Color("ErrorColor"), lineWidth: 3)
                     )
-                                 
+                    
                     if !registrationViewModel.isPassword2Valid && registrationViewModel.allFieldsFilled{
                         Text("Passwords do not match.")
                             .foregroundColor(Color.white)
-                        .font(.system(size: 17))
-                        .padding(.leading, 5)
-                                 }
+                            .font(.system(size: 17))
+                            .padding(.leading, 5)
+                    }
                 }
                 if registrationViewModel.showGlobalError {
                     Text(registrationViewModel.globalErrorMessage)
                         .foregroundColor(.white)
-                    .font(.system(size: 17))
+                        .font(.system(size: 17))
                 }
                 
                 Button(action: {
@@ -119,23 +117,14 @@ struct RegistrationView: View {
                         .background(Color("LoginButton"))
                         .cornerRadius(20)
                         .bold()
-                        
-                    
                 }
                 .padding(.top, 40)
-                
-                
-        
-                
             }
-            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .ignoresSafeArea()
         .applyGlobalBackground()
-        
     }
-   
 }
 
 
