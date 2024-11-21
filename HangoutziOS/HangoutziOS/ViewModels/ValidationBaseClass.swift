4471e2790702417747986ef0cb90a86af70eff09
alex64a <aleksamitrovcan@gmail.com>
2024-11-21 13:20:49 +0100

    Fixed error when changed constants

--- HangoutziOS/HangoutziOS/ViewModels/ValidationBaseClass.swift
+++ HangoutziOS/HangoutziOS/ViewModels/ValidationBaseClass.swift
@@ -10,20 +10,20 @@ import Foundation
 class Validation {
     
     func isValidEmail(_ email: String) -> Bool {
-        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", Constants.emailRegex)
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", ValidationConstants.emailRegex)
         return emailPredicate.evaluate(with: email)
     }
     func isEmpty(_ text: String) -> Bool {
         return text.trimmingCharacters(in: .whitespaces).isEmpty
     }
     func isValidPassword(_ password: String) -> Bool {
-        let passPredicate = NSPredicate(format: "SELF MATCHES %@", Constants.passRegex)
+        let passPredicate = NSPredicate(format: "SELF MATCHES %@", ValidationConstants.passRegex)
         return passPredicate.evaluate(with: password)
     }
     func isConfirmPasswordEqual(pass: String, conPass: String) -> Bool {
         return pass == conPass
     }
     func isNameValid(_ name: String) -> Bool {
-        return name.count >= Constants.minNameLength && name.count <= Constants.maxNameLength
+        return name.count >= ValidationConstants.minNameLength && name.count <= ValidationConstants.maxNameLength
     }
 }
