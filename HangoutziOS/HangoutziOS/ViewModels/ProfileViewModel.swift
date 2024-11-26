b446cd0ae652df1b4329f1f8d53110f0c19a0042
alex64a <aleksamitrovcan@gmail.com>
2024-11-26 11:49:28 +0100

    Finished implementing PR comments in code. Added AccessibilityIdentifiersConstants to Constants/ folder. Removed uneccessary spaces.

--- HangoutziOS/HangoutziOS/ViewModels/ProfileViewModel.swift
+++ HangoutziOS/HangoutziOS/ViewModels/ProfileViewModel.swift
@@ -14,7 +14,7 @@ class ProfileViewModel: ObservableObject {
     @Published var urlGetUserEmail: String = ""
     
     func createUrlToGetAvatarJson(id: String){
-        urlGetAvatarJson = SupabaseConfig.baseURL + "rest/v1/users?select=avatar&id=eq.\(id)"
+        urlGetAvatarJson = SupabaseConfig.baseURL + SupabaseConstants.SELECT_AVATAR + "\(id)"
     }
     func createUrlToGetAvatarPhoto(imageName: String?) {
         urlGetAvatarPhoto = SupabaseConfig.baseURLStorage + "\(imageName ?? "error")"
